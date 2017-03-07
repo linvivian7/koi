9d0894ec8a138f40f46f0e86aa429f94d6000601
Vivian Lin <linvivian7@gmail.com>
2017-03-07 14:18:32 -0800

    Edit PORT

--- server.py
+++ server.py
@@ -574,4 +574,6 @@ if __name__ == "__main__":
     app.jinja_env.auto_reload = app.debug  # make sure templates, etc. are not cached in debug mode
 
     connect_to_db(app)
-    app.run(port=5000, host='0.0.0.0')
+    PORT = int(os.environ.get("PORT", 5000))
+
+    app.run(host="0.0.0.0", port=PORT)
